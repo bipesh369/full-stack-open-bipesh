67146715a42a3dea8a7d602877bdcdd3dbf09b98
bipesh369 <juniorbipesh@gmail.com>
2025-08-03 10:50:21 +0000

    Complete part1 exercise

---
+++ part1/courseinfo/src/Total.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Total(props) {
+  return (
+    <>
+    <div> 
+      Number of Exercises:{props.exercises1 + props.exercises2 + props.exercises3}
+  
+    </div>
+
+   
+    </>
+  )
+}
+
+export default Total
